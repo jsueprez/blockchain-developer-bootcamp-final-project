da6f10fef0bf3fe839e96aa449dd5f2ccbeae6d9
jsueprez <ajperez502@gmail.com>
2021-11-22 23:52:54 +0100

    refactoring unit-testing

--- contracts/CriolloToken.sol
+++ contracts/CriolloToken.sol
@@ -8,34 +8,95 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract CriolloToken is ERC721, Ownable {
     using Counters for Counters.Counter;
 
-    address payable public _owner;
-    Counters.Counter private _tokenIdCounter;
+    event Purchase(address owner, uint256 price, uint256 id);
+    event TokenUnlocked(uint256 _id, address _unlockerAddress);
+    event NewTokenMinted(uint256 _tokenId);
 
+    enum State {
+        NotListed,
+        ForSale,
+        Locked,
+        Shipped,
+        Unlocked
+    }
+    struct Criollo {
+        uint256 tokenId;
+        uint256 price;
+        State state;
+        address payable tokenOwner;
+    }
+
+    address payable public _owner;
     string public baseURI;
     string public baseExtension = ".json";
     uint256 public cost = 0.25 ether;
     uint256 public maxMintAmount = 1;
 
-    enum State {
-        NotListed,
-        ForSale,
-        SoldAndLocked,
-        SoldAndShipped,
-        SoldAndUnlocked
+    Counters.Counter private _tokenIdCounter;
+
+    mapping(uint256 => Criollo) criollos;
+
+    modifier isMinted(uint256 _id) {
+        require(
+            _exists(_id),
+            "CriolloToken: Error, This token has not been minted yet"
+        );
+        _;
+    }
+    modifier isOwner(uint256 _id) {
+        require(
+            ownerOf(_id) == msg.sender,
+            "CriolloToken: Error, You are not the token owner!"
+        );
+        _;
     }
 
-    // internal
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseURI;
+    modifier canBeListed(uint256 _id) {
+        require(
+            criollos[_id].state == State.NotListed ||
+                criollos[_id].state == State.Unlocked,
+            "CriolloToken: Error, This token can not be listed!"
+        );
+        _;
     }
 
-    event Purchase(address owner, uint256 price, uint256 id);
+    modifier isUnlocked(uint256 _id) {
+        require(
+            criollos[_id].state == State.Unlocked,
+            "CriolloToken: Error, This token is not Unlocked !"
+        );
+        _;
+    }
 
-    mapping(uint256 => State) public state;
-    mapping(uint256 => uint256) public price;
-    mapping(uint256 => uint256) public tokenLockedFromTimestamp;
+    modifier isLocked(uint256 _id) {
+        require(
+            criollos[_id].state == State.Locked,
+            "CriolloToken: Error, This token has not been Locked!"
+        );
+        _;
+    }
 
-    event TokenUnlocked(uint256 _id, address _unlockerAddress);
+    modifier isShipped(uint256 _id) {
+        require(
+            criollos[_id].state == State.Shipped,
+            "CriolloToken: Error, This token has not been Shipped!"
+        );
+        _;
+    }
+
+    modifier forSale(uint256 _id) {
+        require(
+            criollos[_id].state == State.ForSale ||
+                criollos[_id].state == State.Unlocked,
+            "CriolloToken: Error, This token is not for sale"
+        );
+        _;
+    }
+
+    modifier paidEnough(uint256 _price) {
+        require(msg.value >= _price, "CriolloToken: Error, Token cost more");
+        _;
+    }
 
     constructor(
         string memory name_,
@@ -46,42 +107,8 @@ contract CriolloToken is ERC721, Ownable {
         setBaseURI(_initBaseURI);
     }
 
-    function unlockToken(uint256 _id) public isShipped(_id) {
-        require(
-            msg.sender == ownerOf(_id),
-            "CriolloToken: Only the Owner can unlock the Token"
-        );
-        state[_id] = State.SoldAndUnlocked;
-        emit TokenUnlocked(_id, msg.sender);
-    }
-
-    modifier onlyForSaleTokens(uint256 _id) {
-        require(
-            state[_id] == State.ForSale,
-            "CriolloToken: This Token is not For sale!!"
-        );
-        _;
-    }
-
-    modifier isShipped(uint256 _id) {
-        require(state[_id] == State.SoldAndShipped);
-        _;
-    }
-
-    modifier onlyLockedTokens(uint256 _id) {
-        require(
-            state[_id] == State.SoldAndLocked,
-            "CriolloToken: This Token has not been sold yet!!"
-        );
-        _;
-    }
-
-    modifier onlyUnlockedTokens(uint256 _id) {
-        require(
-            state[_id] == State.SoldAndUnlocked,
-            "CriolloToken: This Token is not Unlocked!!"
-        );
-        _;
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
     }
 
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
@@ -95,6 +122,74 @@ contract CriolloToken is ERC721, Ownable {
         baseExtension = _newBaseExtension;
     }
 
+    function safeMint(uint256 _price) public payable onlyOwner {
+        uint256 _tokenId = _tokenIdCounter.current();
+
+        _safeMint(msg.sender, _tokenId);
+
+        criollos[_tokenId] = Criollo({
+            price: _price,
+            tokenId: _tokenId,
+            state: State.NotListed,
+            tokenOwner: _owner
+        });
+
+        emit NewTokenMinted(_tokenId);
+
+        _tokenIdCounter.increment();
+    }
+
+    function addToMarketPlace(uint256 _id, uint256 _price)
+        public
+        isMinted(_id)
+        isOwner(_id)
+        canBeListed(_id)
+    {
+        criollos[_id].state = State.ForSale;
+        criollos[_id].price = _price;
+    }
+
+    function getCriollo(uint256 _tokenId)
+        public
+        view
+        returns (
+            uint256 tokenId,
+            uint256 price,
+            uint256 state,
+            address tokenOwner
+        )
+    {
+        tokenId = criollos[_tokenId].tokenId;
+        price = criollos[_tokenId].price;
+        state = uint256(criollos[_tokenId].state);
+        tokenOwner = criollos[_tokenId].tokenOwner;
+        return (tokenId, price, state, tokenOwner);
+    }
+
+    function buy(uint256 _id)
+        external
+        payable
+        isMinted(_id)
+        forSale(_id)
+        paidEnough(criollos[_id].price)
+    {
+        if (criollos[_id].tokenOwner == _owner) {
+            _buyFromCriollo(_id);
+        } else {
+            _buyFromUser(_id);
+        }
+
+        emit Purchase(msg.sender, criollos[_id].price, criollos[_id].tokenId);
+    }
+
+    function shipped(uint256 _id) public onlyOwner isMinted(_id) isLocked(_id) {
+        criollos[_id].state = State.Shipped;
+    }
+
+    function unlockToken(uint256 _id) public isShipped(_id) isOwner(_id) {
+        criollos[_id].state = State.Unlocked;
+    }
+
     function withdraw() public payable onlyOwner {
         (bool success, ) = payable(msg.sender).call{
             value: address(this).balance
@@ -102,78 +197,19 @@ contract CriolloToken is ERC721, Ownable {
         require(success);
     }
 
-    function markAsShipped(uint256 _id) public onlyOwner onlyLockedTokens(_id) {
-        require(
-            _exists(_id),
-            "ERC721: Error, This token has not been minted yet"
-        );
-        state[_id] = State.SoldAndShipped;
+    function _buyFromCriollo(uint256 _id) internal {
+        _transfer(_owner, msg.sender, _id);
+        _owner.transfer(msg.value);
+
+        criollos[_id].tokenOwner = payable(msg.sender);
+        criollos[_id].state = State.Locked;
     }
 
-    function listTokenForSale(uint256 _id) public onlyOwner {
-        require(
-            _exists(_id),
-            "ERC721: Error, This token has not been minted yet"
-        );
-        require(
-            ownerOf(_id) == _owner,
-            "CriolloToken: Criollo is not the owner of this token anymore!"
-        );
-        state[_id] = State.ForSale;
-    }
+    function _buyFromUser(uint256 _id) internal isUnlocked(_id) {
+        address payable _currentOwner = criollos[_id].tokenOwner;
 
-    function safeMint(uint256 _price) public payable onlyOwner {
-        uint256 _tokenID = _tokenIdCounter.current();
-
-        _safeMint(msg.sender, _tokenID);
-        _tokenIdCounter.increment();
-
-        price[_tokenID] = _price;
-    }
-
-    modifier isforSale(uint256 _id) {
-        require(state[_id] == State.ForSale);
-        _;
-    }
-
-    function buy(uint256 _id) external payable isforSale(_id) {
-        require(_exists(_id), "Error, This token has not been minted yet"); //not exists
-        require(
-            ownerOf(_id) == _owner,
-            'CriolloToken: This token does not belong to Criollo, please use the "trade" function if you want to buy this token.'
-        );
-
-        require(msg.value >= price[_id], "Error, Token costs more"); //costs more
-
-        _buy(_id); //buying to contract owner
-        emit Purchase(msg.sender, price[_id], _id);
-    }
-
-    function trade(uint256 _id) external payable onlyUnlockedTokens(_id) {
-        require(_exists(_id), "Error, This token has not been minted yet"); //not exists
-        require(
-            ownerOf(_id) != _owner,
-            'CriolloToken: This token does belong to Criollo, please use the "buy" function if you want to buy this token.'
-        );
-        require(msg.value >= price[_id], "Error, Token costs more"); //costs more
-        _trade(_id); //buying to user
-    }
-
-    function _validate(uint256 _id) internal {
-        require(_exists(_id), "Error, This token has not been minted yet"); //not exists
-        require(msg.value >= price[_id], "Error, Token costs more"); //costs more
-    }
-
-    function _buy(uint256 _id) internal {
-        _transfer(_owner, msg.sender, _id); //nft to user
-        state[_id] = State.SoldAndLocked;
-    }
-
-    function _trade(uint256 _id) internal onlyUnlockedTokens(_id) {
-        address payable _currentOwner = payable(ERC721.ownerOf(_id));
-
-        _transfer(ERC721.ownerOf(_id), msg.sender, _id); //nft to user
-
-        _currentOwner.transfer(msg.value); //eth to owner
+        _transfer(_currentOwner, msg.sender, _id);
+        _currentOwner.transfer(msg.value);
+        criollos[_id].tokenOwner = payable(msg.sender);
     }
 }
--- test/criollo_token.js
+++ test/criollo_token.js
@@ -1,3 +1,4 @@
+let BN = web3.utils.BN;
 const CriolloToken = artifacts.require("CriolloToken");
 let { catchRevert } = require("./exceptionsHelpers.js");
 
@@ -7,7 +8,9 @@ contract("CriolloToken", function (accounts) {
     const tokenName = "Criollo Token";
     const symbol = "CRI";
     const initBaseURI = "someURI";
-    const [owner, otherUser] = accounts;
+    const [owner, alice, bob] = accounts;
+    const initialPrice = 1;
+    const emptyAddress = "0x0000000000000000000000000000000000000000";
 
     beforeEach(async () => {
         instance = await CriolloToken.new(tokenName, symbol, initBaseURI, { from: owner });
@@ -19,161 +22,273 @@ contract("CriolloToken", function (accounts) {
         //expect(await instance._initBaseURI).to.equal(initBaseURI);
     });
 
-    describe("Criollo minting process", () => {
+    describe("Criollo minting process,,,,", () => {
 
         it("should revert when someone who is not the contract owner try to mint tokens", async () => {
-            await catchRevert(instance.safeMint(1, { from: otherUser }));
+            await catchRevert(instance.safeMint(initialPrice, { from: alice }));
         })
 
         it("should the contract owner be able to mint any token", async () => {
-            await instance.safeMint(1, { from: owner })
-            const tokensMinted = await instance.balanceOf(owner);
+            await instance.safeMint(initialPrice, { from: owner })
 
-            assert.equal(1, tokensMinted, "The owner should be able to Mint any tokens");
+            const newToken = await instance.getCriollo.call(0);
+
+            assert.equal(
+                newToken[1].toString(10),
+                initialPrice,
+                'the price of the last added token does not match the expected value',
+            );
+
+            assert.equal(
+                newToken[2].toString(10),
+                CriolloToken.State.NotListed,
+                'the state of the token should be "Not Listed"',
+            );
+
+            assert.equal(
+                newToken[3],
+                owner,
+                "the address minting the token should be listed as the token owner",
+            );
         })
 
-        it("should a token get a price and a State equal to NotListed when minted", async () => {
-            await instance.safeMint(1, { from: owner })
-            const tokensMinted = await instance.balanceOf(owner);
-            const state = await instance.state(0);
+        it("should emit a NewTokenMinted event when an token is minted", async () => {
+            let eventEmitted = false;
+            const tx = await instance.safeMint(initialPrice, { from: owner });
+            if (tx.logs[1].event == "NewTokenMinted") {
+                eventEmitted = true;
+            }
 
-            assert.equal(1, tokensMinted, "The owner should be able to Mint any tokens");
-            assert.equal(0, state, "The state of the token should be Not Listed");
-        })
-
+            assert.equal(
+                eventEmitted,
+                true,
+                "minting an token should emit a Minting event",
+            );
+        });
     })
 
-    describe("Criollo listing a token for sale", () => {
-        before(async () => {
-            await instance.safeMint(1, { from: owner });
+    describe("Criollo listing a token to MarketPlace...", () => {
+        const listingPrice = 2;
+
+        beforeEach(async () => {
+            await instance.safeMint(initialPrice, { from: owner });
         })
 
-        it("should revert if the contract owner try to list a non-exiting token for sale ", async () => {
-            await catchRevert(instance.listTokenForSale(0));
+        it("should revert if the token owner try to list a non-exiting token for sale ", async () => {
+            await catchRevert(instance.addToMarketPlace(1, listingPrice));
         })
 
-        it("should revert if someone else who is not the contract owner try to list a token for sale", async () => {
-            await catchRevert(instance.listTokenForSale(0, { from: otherUser }));
+        it("should revert if an user try to list a token which does not own to the market place", async () => {
+            await catchRevert(instance.addToMarketPlace(0, listingPrice, { from: alice }));
         })
 
-        it("should the contract owner be able to list tokens for sale", async () => {
-            await instance.safeMint(1, { from: owner });
-            await instance.listTokenForSale(0);
-            const state = await instance.state(0);
+        it("should update the state accordingly when a token is listed succesfully to the market place", async () => {
 
-            assert.equal(1, state, "The state of the token should be ForSale");
+            await instance.addToMarketPlace(0, listingPrice, { from: owner });
+
+            const token = await instance.getCriollo.call(0);
+
+            assert.equal(
+                token[2].toString(10),
+                CriolloToken.State.ForSale,
+                'the state of the token should be "For Sale"',
+            );
+
+            assert.equal(
+                token[1].toString(10),
+                listingPrice,
+                'the price of the token should be 2'
+            );
         })
-
     })
 
-    describe("WorkFlow Users buying process", () => {
+    describe("Alice buying a token in the MarketPlace...", () => {
+        const listingPrice = 2;
+
+        beforeEach(async () => {
+            await instance.safeMint(initialPrice, { from: owner });
+            await instance.safeMint(initialPrice, { from: owner });
+            await instance.addToMarketPlace(0, listingPrice, { from: owner });
+        })
+
         it("should revert when users try to buy a token that does not exist", async () => {
-            await catchRevert(instance.buy(1, { from: otherUser, value: 1 }));
+            await catchRevert(instance.buy(1, { from: alice, value: 1 }));
         })
-
-        /*it("should revert when users try to use buy function to trade a token", async () => {
-            await catchRevert(instance.buy(1, { from: owner, value: priceOfContractOwner }));
-        })*/
 
         it("should revert when users try to buy a token that is not for sale", async () => {
-            await instance.safeMint(1, { from: owner });
-            await catchRevert(instance.buy(0, { from: otherUser, value: 1 }));
+            await catchRevert(instance.buy(1, { from: alice, value: 1 }));
         })
 
         it("should revert when not enough value is sent when purchasing a token", async () => {
-            const priceOfOtherUser = 0;
-            await instance.safeMint(1, { from: owner });
-            await instance.listTokenForSale(0, { from: owner });
-
-            await catchRevert(instance.buy(0, { from: otherUser, value: priceOfOtherUser }));
+            const priceOfalice = 1;
+            await catchRevert(instance.buy(0, { from: alice, value: priceOfalice }));
         })
 
-        it("should set state to SoldAndLocked after token been sold and been transfer to new owner", async () => {
-            const priceOfOtherUser = 1;
-            await instance.safeMint(1, { from: owner });
-            await instance.listTokenForSale(0, { from: owner });
-            await instance.buy(0, { from: otherUser, value: priceOfOtherUser })
+        it("should allow someone to purchase a token and update state accordingly when the token owner is Criollo", async () => {
+            const priceOfalice = "2";
 
-            const state = await instance.state(0);
-            const newOwner = await instance.ownerOf(0);
+            var ownerBalanceBefore = await web3.eth.getBalance(owner);
+            var aliceBalanceBefore = await web3.eth.getBalance(alice);
 
-            assert.equal(2, state, "The state of the token should be SoldAndLocked");
-            assert.equal(newOwner, otherUser, "The token should be own by the otherUser");
-        })
-    })
+            await instance.buy(0, { from: alice, value: priceOfalice })
 
-    describe("WorkFlow Criollo shipping the chocolate", () => {
+            var ownerBalanceAfter = await web3.eth.getBalance(owner);
+            var aliceBalanceAfter = await web3.eth.getBalance(alice);
 
-        it("should revert when try to markAsShipped a token which does not exist", async () => {
-            const priceOfOtherUser = 1;
-            await instance.safeMint(1, { from: owner });
-            await instance.listTokenForSale(0, { from: owner });
-            await instance.buy(0, { from: otherUser, value: priceOfOtherUser })
-            await catchRevert(instance.markAsShipped(1, { from: owner }));
-        })
+            const token = await instance.getCriollo.call(0);
 
-        it("should revert when try to markAsShipped a token which is not SoldAndLocked", async () => {
-            const priceOfOtherUser = 1;
-            await instance.safeMint(1, { from: owner });
-            await instance.listTokenForSale(0, { from: owner });
-            await catchRevert(instance.markAsShipped(1, { from: owner }));
-        })
+            assert.equal(
+                token[2].toString(10),
+                CriolloToken.State.Locked,
+                'the state of the item should be "Locked"',
+            );
 
-        it("should revert when otherUser try to markAsShipped a token", async () => {
-            const priceOfOtherUser = 1;
-            await instance.safeMint(1, { from: owner });
-            await instance.listTokenForSale(0, { from: owner });
-            await catchRevert(instance.markAsShipped(0, { from: otherUser }));
-        })
+            assert.equal(
+                token[3].toString(10),
+                alice,
+                'the new token owner should be alice',
+            );
 
-        it("should be able to markAsShipped a token which was sold to an user", async () => {
-            const priceOfOtherUser = 1;
-            await instance.safeMint(1, { from: owner });
-            await instance.listTokenForSale(0, { from: owner });
-            await instance.buy(0, { from: otherUser, value: priceOfOtherUser })
-            await instance.markAsShipped(0, { from: owner });
+            assert.equal(
+                new BN(ownerBalanceAfter).toString(),
+                new BN(ownerBalanceBefore).add(new BN(priceOfalice)).toString(),
+                "owner's balance should be increased by the price of the item",
+            );
 
-            const state = await instance.state(0);
-            assert.equal(3, state, "The state of the token should be SoldAndShipped");
+            assert.isBelow(
+                Number(aliceBalanceAfter),
+                Number(new BN(aliceBalanceBefore).sub(new BN(priceOfalice))),
+                "alice's balance should be reduced by more than the price of the item (including gas costs)",
+            );
         })
     })
 
-    /*describe("WorkFlow Users unlocking the token", () => {
+    describe("Criollo shipping the chocolate...", () => {
+        const listingPrice = 2;
+        const priceOfalice = "2";
+
+        beforeEach(async () => {
+            await instance.safeMint(initialPrice, { from: owner });
+            await instance.addToMarketPlace(0, listingPrice, { from: owner });
+            await instance.buy(0, { from: alice, value: priceOfalice })
+        })
+
+        it("should revert when try to ship a token which does not exist", async () => {
+            await catchRevert(instance.shipped(1, { from: owner }));
+        })
+
+        it("should revert when try to ship a token which is not SoldAndLocked", async () => {
+            await catchRevert(instance.shipped(1, { from: owner }));
+        })
+
+        it("should revert when alice or bob try to ship a token", async () => {
+            await catchRevert(instance.shipped(0, { from: alice }));
+            await catchRevert(instance.shipped(0, { from: bob }));
+        })
+
+        it("should be able to ship a token which was sold to bob and update state accordingly", async () => {
+            await instance.shipped(0, { from: owner });
+
+            const token = await instance.getCriollo.call(0);
+
+            assert.equal(
+                token[2].toString(10),
+                CriolloToken.State.Shipped,
+                'the state of the item should be "Shipped"',
+            );
+        })
+    })
+
+    describe("Alice unlocking the token when receiving the physical asset...", () => {
+        const listingPrice = 2;
+        const priceOfalice = 2;
+
+        beforeEach(async () => {
+            await instance.safeMint(initialPrice, { from: owner });
+            await instance.safeMint(initialPrice, { from: owner });
+            await instance.addToMarketPlace(0, listingPrice, { from: owner });
+            await instance.addToMarketPlace(1, listingPrice, { from: owner });
+            await instance.buy(0, { from: alice, value: priceOfalice })
+            await instance.buy(1, { from: alice, value: priceOfalice })
+            await instance.shipped(0, { from: owner });
+        })
 
         it("should revert when try to Unlock a token which is not shipped", async () => {
-            const priceOfOtherUser = 1;
-            await instance.safeMint(1, { from: owner });
-            await instance.listTokenForSale(0, { from: owner });
-            await instance.buy(0, { from: otherUser, value: priceOfOtherUser })
-            await instance.markAsShipped(1, { from: owner });
+            await catchRevert(instance.unlockToken(1, { from: alice }));
         })
 
-        it("should revert when try to markAsShipped a token which is not SoldAndLocked", async () => {
-            const priceOfOtherUser = 1;
-            await instance.safeMint(1, { from: owner });
-            await instance.listTokenForSale(0, { from: owner });
-            await catchRevert(instance.markAsShipped(1, { from: owner }));
+        it("should revert when try to Unlock a token when your are not the token owner", async () => {
+            await catchRevert(instance.unlockToken(0, { from: bob }));
         })
 
-        it("should revert when otherUser try to markAsShipped a token", async () => {
-            const priceOfOtherUser = 1;
-            await instance.safeMint(1, { from: owner });
-            await instance.listTokenForSale(0, { from: owner });
-            await catchRevert(instance.markAsShipped(0, { from: otherUser }));
+        it("should be able to Unlock when received and update the state accordingly", async () => {
+            await instance.unlockToken(0, { from: alice });
+
+            const token = await instance.getCriollo.call(0);
+
+            assert.equal(
+                token[2].toString(10),
+                CriolloToken.State.Unlocked,
+                'the state of the item should be "Unlocked"',
+            );
+        })
+    })
+
+    describe("Bob buying an Alice's token...", () => {
+        const listingPrice = 2;
+        const priceOfalice = 2;
+
+        beforeEach(async () => {
+            await instance.safeMint(initialPrice, { from: owner });
+            await instance.safeMint(initialPrice, { from: owner });
+            await instance.addToMarketPlace(0, listingPrice, { from: owner });
+            await instance.addToMarketPlace(1, listingPrice, { from: owner });
+            await instance.buy(0, { from: alice, value: priceOfalice })
+            await instance.buy(1, { from: alice, value: priceOfalice })
+            await instance.shipped(0, { from: owner });
+            await instance.shipped(1, { from: owner });
+            await instance.unlockToken(0, { from: alice });
         })
 
-        it("should be able to markAsShipped a token which was sold to an user", async () => {
-            const priceOfOtherUser = 1;
-            await instance.safeMint(1, { from: owner });
-            await instance.listTokenForSale(0, { from: owner });
-            await instance.buy(0, { from: otherUser, value: priceOfOtherUser })
-            await instance.markAsShipped(0, { from: owner });
-
-            const state = await instance.state(0);
-            assert.equal(3, state, "The state of the token should be SoldAndShipped");
+        it("should revert when Bob try to buy a token to Alice and the token is not Unlocked", async () => {
+            await catchRevert(instance.buy(1, { from: bob, value: listingPrice }));
         })
 
+        it("should allow Bob to buy a token to Alice and update state accordingly", async () => {
 
-    })*/
+            var bobBalanceBefore = await web3.eth.getBalance(bob);
+            var aliceBalanceBefore = await web3.eth.getBalance(alice);
 
+            await instance.buy(0, { from: bob, value: priceOfalice })
+
+            var bobBalanceAfter = await web3.eth.getBalance(bob);
+            var aliceBalanceAfter = await web3.eth.getBalance(alice);
+
+            const token = await instance.getCriollo.call(0);
+
+            assert.equal(
+                token[2].toString(10),
+                CriolloToken.State.Unlocked,
+                'the state of the item should stay "Unlocked"',
+            );
+
+            assert.equal(
+                token[3].toString(10),
+                bob,
+                'the new token owner should be bob',
+            );
+
+            assert.equal(
+                new BN(aliceBalanceAfter).toString(),
+                new BN(aliceBalanceBefore).add(new BN(priceOfalice)).toString(),
+                "alice's balance should be increased by the price of the item",
+            );
+
+            assert.isBelow(
+                Number(bobBalanceAfter),
+                Number(new BN(bobBalanceBefore).sub(new BN(priceOfalice))),
+                "bob's balance should be reduced by more than the price of the item (including gas costs)",
+            );
+        })
+    })
 })
