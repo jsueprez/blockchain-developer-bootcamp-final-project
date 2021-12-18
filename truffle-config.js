86beb32cf2e8a3b883e11d2ab83599a357667026
jsueprez <ajperez502@gmail.com>
2021-12-18 22:34:54 +0100

    changes

--- truffle-config.js
+++ truffle-config.js
@@ -10,11 +10,6 @@ module.exports = {
 
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    development: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 7545, // Standard Ethereum port (default: none)
-      network_id: "1337", // Any network (default: none)
-    },
     ganache_gui: {
       host: "127.0.0.1",
       port: 7545,
